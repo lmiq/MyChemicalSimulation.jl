a618c9cc51928882461fbe186221b9f3d1d90c8e
Leandro Martinez <lmartine@unicamp.br>
2025-05-19 08:56:16 -0300

    up

--- src/MyChemicalSimulation.jl
+++ src/MyChemicalSimulation.jl
@@ -19,6 +19,7 @@ end
     N::Int = sum(N0)
     kvec::Vector{Float64} = [0.5, 0.5]
     temperature::Float32 = 298.15
+    enthalpy::Float32 = 0.0
     cutoff::Float32 = 3.0f0
     box_size::Float32 = 2 * sqrt(N) * cutoff
     dt::Float32 = 1.0f0
@@ -34,12 +35,7 @@ end
     	[ParticleState(;color=colors[4], type=:star5) for _ in 1:N0[4]],
     )
     current_state::Vector{ParticleState} = deepcopy(initial_states)
-    N_over_time::Vector{Vector{Int}} = [
-        [i == 1 ? N0[1] : 0 for i in 1:nsteps],
-        [i == 1 ? N0[2] : 0 for i in 1:nsteps],
-        [i == 1 ? N0[3] : 0 for i in 1:nsteps],
-        [i == 1 ? N0[4] : 0 for i in 1:nsteps],
-    ]
+    N_over_time::Vector{Vector{Int}} = [ [N0[1]], [N0[2]], [N0[3]], [N0[4]] ]
     stop::Bool = false
 end
 SimulationData(sim::SimulationData) =
@@ -56,7 +52,11 @@ function get_N(sim)
 end
 
 function compute_Q(sim)
-    N1, N2, N3, N4 = get_N(sim)
+    ihalf = max(1,div(length(sim.N_over_time[1]),2))
+    N1 = mean(sim.N_over_time[1][ihalf:end])
+    N2 = mean(sim.N_over_time[2][ihalf:end])
+    N3 = mean(sim.N_over_time[3][ihalf:end])
+    N4 = mean(sim.N_over_time[4][ihalf:end])
     return (N3*N4)/(N1*N2)
 end
 
@@ -76,14 +76,14 @@ function simulate()
     #
     # Setup
     #
-    fig = Figure(size=(1200, 800))
+    fig = Figure(size=(1400, 700))
     fig[1:2,1] = setup_grid = GridLayout(tellwidth=false)
 
     cgrid = setup_grid[1:8, 1] = [
         Label(fig, "Temperatura:", halign=:right),
         Label(fig, "k₁:", halign=:right),
         Label(fig, "k₂:", halign=:right),
-        Menu(fig, options = ["Azul", "Transparente"]),
+        Menu(fig, options = ["Azul", "Transparente"], halign=:right),
         Menu(fig, options = ["Vermelho", "Transparente"]),
         Menu(fig, options = ["Laranja", "Transparente"]),
         Menu(fig, options = ["Verde", "Transparente"]),
@@ -120,15 +120,16 @@ function simulate()
         Label(fig, "", halign=:left),
         Label(fig, "min", halign=:left),
     ]
+    tbw = 60 
     tb = setup_grid[1:8, 2] = [
-        Textbox(fig, placeholder=@lift(string(round($(obs).temperature; digits=2))), validator=Float64, width=100, halign=:center),
-        Textbox(fig, placeholder=@lift(string(round($(obs).kvec[1]; digits=2))), validator=Float64, width=100, halign=:center),
-        Textbox(fig, placeholder=@lift(string(round($(obs).kvec[2]; digits=2))), validator=Float64, width=100, halign=:center),
-        Textbox(fig, placeholder=@lift(string($(obs).N0[1])), validator=Int, width=100),
-        Textbox(fig, placeholder=@lift(string($(obs).N0[2])), validator=Int, width=100),
-        Textbox(fig, placeholder=@lift(string($(obs).N0[3])), validator=Int, width=100),
-        Textbox(fig, placeholder=@lift(string($(obs).N0[4])), validator=Int, width=100),
-        Textbox(fig, placeholder=@lift(string($(obs).time)), validator=Float64, width=100),
+        Textbox(fig, placeholder=@lift(string(round($(obs).temperature; digits=2))), validator=Float64, width=tbw),
+        Textbox(fig, placeholder=@lift(string(round($(obs).kvec[1]; digits=2))), validator=Float64, width=tbw),
+        Textbox(fig, placeholder=@lift(string(round($(obs).kvec[2]; digits=2))), validator=Float64, width=tbw),
+        Textbox(fig, placeholder=@lift(string($(obs).N0[1])), validator=Int, width=tbw),
+        Textbox(fig, placeholder=@lift(string($(obs).N0[2])), validator=Int, width=tbw),
+        Textbox(fig, placeholder=@lift(string($(obs).N0[3])), validator=Int, width=tbw),
+        Textbox(fig, placeholder=@lift(string($(obs).N0[4])), validator=Int, width=tbw),
+        Textbox(fig, placeholder=@lift(string($(obs).time)), validator=Float64, width=tbw),
     ]
     on(tb[1].stored_string) do s
         up!(obs, :temperature, parse(Float32, s))
@@ -172,19 +173,24 @@ function simulate()
         up!(obs, :stop, true)
     end
 
-    colsize!(setup_grid, 1, Relative(4/10))
-    colsize!(setup_grid, 2, Relative(4/10))
-    colsize!(setup_grid, 3, Relative(2/10))
+    colsize!(setup_grid, 1, Fixed(90))
+    colsize!(setup_grid, 2, Fixed(60))
+    colsize!(setup_grid, 3, Fixed(80))
 
     # Figure layout
-    Axis(fig[1:2,2], title=@lift("k₁ = "*string($(obs).kvec[1])*" k₂ = "*string($(obs).kvec[2])*" - Step = "*string($(obs).step)))
+    ax = Axis(fig[1:2,2], title=@lift(
+            "k₁ = "*string(round($(obs).kvec[1]; digits=3))*
+            " k₂ = "*string(round($(obs).kvec[2]; digits=3))*" - Step = "*string($(obs).step))
+        )
+    hidedecorations!(ax)
+
     Axis(fig[1,3]; 
         title=@lift("Histograma - N₀ = "*string($(obs).N0)),
         xlabel="Tipo",
         ylabel="Número",
         limits=@lift((0, 5, 0, yscale($obs))),
     )
-    text!(fig[1,3], @lift("Q = "*string(round(compute_Q($obs);digits=5))), position=@lift((4.0, 0.95*yscale($obs))))
+    text!(fig[1,3], @lift("Q = "*string(round(compute_Q($obs);digits=5))), position=@lift((3.8, 0.90*yscale($obs))))
 
     Axis(fig[2,3], 
         title=@lift("Número de Moléculas - N = ["*join(get_N($obs), ", ")*"]"),
@@ -196,7 +202,7 @@ function simulate()
     # Setup simulation and plots
     setup!(fig, obs)
 
-    colsize!(fig.layout, 1, Relative(2/10))
+    colsize!(fig.layout, 1, Fixed(240))
     colsize!(fig.layout, 2, Relative(5/10))
     colsize!(fig.layout, 3, Relative(3/10))
 
@@ -241,7 +247,7 @@ function setup!(fig, obs)
     #
     for ic in eachindex(sim.colors)
         scatter!(fig[2,3], 
-            @lift([(i*$(obs).time/$(obs).nsteps, $(obs).N_over_time[ic][i]) for i in 1:$(obs).nsteps]),
+            @lift([(i*$(obs).time/$(obs).nsteps, $(obs).N_over_time[ic][i]) for i in 1:length($(obs).N_over_time[ic])]),
             color=@lift($(obs).colors[ic]),
             markersize=5,
         )
@@ -284,7 +290,7 @@ function simulate!(obs)
         sim.positions .= sys.xpositions
         N_current = get_N(sim)
         for ic in eachindex(sim.colors)
-            sim.N_over_time[ic][istep] = N_current[ic]
+            push!(sim.N_over_time[ic], N_current[ic])
         end
         obs[].stop && break
         obs[] = SimulationData(sim)
