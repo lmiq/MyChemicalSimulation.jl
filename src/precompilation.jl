add91c098c0332e62d55fe12278735f788425096
Leandro Martinez <lmartine@unicamp.br>
2025-05-27 11:52:30 -0300

    precompile 3D

--- src/precompilation.jl
+++ src/precompilation.jl
@@ -5,7 +5,10 @@ using PrecompileTools: @setup_workload, @compile_workload
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
-        fig, obs = simulate(;N0=[10,10,10,10],time=0.001,precompile=true)
+        fig, obs = simulate(;N0=[10,10,10,10],time=0.001,precompile=true,DIM=2)
+        MyChemicalSimulation.setup!(fig, obs)
+        MyChemicalSimulation.simulate!(obs)
+        fig, obs = simulate(;N0=[10,10,10,10],time=0.001,precompile=true,DIM=3)
         MyChemicalSimulation.setup!(fig, obs)
         MyChemicalSimulation.simulate!(obs)
     end
